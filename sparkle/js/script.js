81ef832b9ced87cae11b81a2f9e80133124c45ec
Psenicinii Valerian <pseniciniivalerian@gmail.com>
2023-03-02 18:48:00 +0200

    Adding the begining of my new Sparkle project

---
+++ sparkle/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    // ----------- don't touch -----------
+    $('.counter').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+    // ----------- don't touch -----------
+});
